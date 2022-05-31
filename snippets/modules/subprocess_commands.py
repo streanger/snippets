3dd28e7fbc7d706599c7c30675a80868798c5f94
streanger <pslisz@o2.pl>
2022-05-31 21:42:21 +0200

    new snippets

--- snippets/modules/subprocess_commands.py
+++ snippets/modules/subprocess_commands.py
@@ -1,3 +1,4 @@
+import sys
 import pprint
 import subprocess
 
@@ -26,8 +27,24 @@ def get_wifi_passwords():
     return wifi_dict
     
     
+def open_directory_in_gui(directory):
+    """open directory(file) in gui view
+    
+    useful:
+        https://stackoverflow.com/questions/3022013/windows-cant-find-the-file-on-subprocess-call
+    """
+    if sys.platform == "darwin":
+        opener = "open"
+    elif sys.platform == "win32":
+        opener = "start"
+    else:
+        opener = "xdg-open"
+    subprocess.call([opener, directory], shell=True)
+    return None
+    
+    
 if __name__ == "__main__":
     wifi_dict = get_wifi_passwords()
     pprint.pprint(wifi_dict)
-    
+    # open_directory_in_gui('TEST')
     
